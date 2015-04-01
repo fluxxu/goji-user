474390943eaa14977f3cccb0c7346df61de2eae9
Flux Xu <fluxxu@gmail.com>
2015-04-01 13:14:14 -0400

    Add schema SQL

--- goji-user.go
+++ goji-user.go
@@ -1,5 +1,18 @@
 package user
 
+// Schema
+// CREATE TABLE `user` (
+//   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+//   `email` varchar(255) NOT NULL,
+//   `password` varchar(255) NOT NULL,
+//   `display_name` varchar(255) NOT NULL,
+//   `created_at` datetime DEFAULT NULL,
+//   `updated_at` datetime DEFAULT NULL,
+//   PRIMARY KEY (`id`),
+//   KEY `user_email_index` (`email`)
+// ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+// ALTER TABLE `user` ADD UNIQUE INDEX `email_unique` (`email` ASC);
+
 import (
 	"github.com/fluxxu/goji-auth"
 	"github.com/jmoiron/sqlx"
